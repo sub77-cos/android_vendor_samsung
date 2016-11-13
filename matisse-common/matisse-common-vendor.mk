fae8c07e724152b8c6036a7998f31189f83a57eb
sub77 <sub77@ymail.com>
2016-11-13 05:18:22 +0000

    matisse-common: Add vendor/qcom/binaries

--- matisse-common/matisse-common-vendor.mk
+++ matisse-common/matisse-common-vendor.mk
@@ -223,4 +223,4 @@ PRODUCT_PACKAGES += \
     libtime_genoff \
     TimeService
 
-$(call inherit-product, vendor/qcom/binaries/msm8226/graphics/graphics-vendor.mk)
+$(call inherit-product, vendor/qcom/binaries/msm8974/graphics/graphics-vendor.mk)
